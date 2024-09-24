c1ff63570c6654b19cc1db5956b1d42ca108d30f
chris <chrisjcoates@live.co.uk>
2024-09-24 11:58:07 +0100

    updated main.py file with __name__ == __main__

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from ui import MainWindow
 
-app = QApplication([])
-main_window = MainWindow()
-main_window.show()
-sys.exit(app.exec_())
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
